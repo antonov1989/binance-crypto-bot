f9115e0ea4d4e5d4945c6a26041c9b4ed5ebb6eb
Mykola Antonov <m.antonov@snov.io>
2025-02-12 11:03:43 +0200

    commit

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const TelegramBot = require('node-telegram-bot-api');
 // Загружаем API-ключи из переменных окружения
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const BINANCE_API_KEY = process.env.BINANCE_API_KEY;
 
 const BINANCE_API_URL = 'https://api2.binance.com/api/v3/ticker/price';
 const COINS = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT']; // Укажи свои монеты
@@ -22,6 +23,7 @@ async function getCryptoPrices() {
                     host: "13.38.153.36",
                     port: 80,
                 },
+                headers: { 'X-MBX-APIKEY': BINANCE_API_KEY },
                 httpsAgent: agent,
             });
             prices[coin] = parseFloat(response.data.price);
